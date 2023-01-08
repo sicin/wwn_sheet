5dab0182fedc964ba0c115a6ca1caa995861f8e0
Ryszard Siciński <rysicin@gmail.com>
2023-01-08 02:13:57 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            spacing: {
+                128: '32rem',
+                144: '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            },
+        },
+    },
+    plugins: [],
+}

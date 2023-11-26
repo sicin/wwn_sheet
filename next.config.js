a14f173a34409bef456afaf01a285f5fd06c6d58
Ryszard Siciński <rysicin@gmail.com>
2023-11-26 14:27:24 +0100

    rebuild from scratch

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    experimental: { appDir: true },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
